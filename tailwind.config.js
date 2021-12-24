eef3de2eefd47a9690f14a2f8d5048b4931536db
am1macdonald <am1macdonald@gmail.com>
2021-12-24 08:40:21 -0500

    initial boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/index.js",
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'cabin': ['"Cabin"', 'sans-serif'],
+        'playfair': ['"Playfair Display"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
